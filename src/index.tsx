f27d2795ddc55207e1f0cd774383201abb58a077
Dylan Modell <dylan.modell.promo@gmail.com>
2021-03-06 02:27:51 -0800

    lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
-import Crew from "./screens/Crew";
-import Directors from "./screens/Directors";
-import Home from "./screens/Home";
-import Philanthropy from "./screens/Philanthropy";
-import Synopsis from "./screens/Synopsis";
-import Technology from "./screens/Technology";
-import Treasure from "./screens/Treasure";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Crew" component={Crew} />
-        <Route exact path="/Directors" component={Directors} />
-        <Route exact path="/Philanthropy" component={Philanthropy} />
-        <Route exact path="/Technology" component={Technology} />
-        <Route exact path="/Synopsis" component={Synopsis} />
-        <Route exact path="/Treasure" component={Treasure} />
-      </Switch>
-    </Router>
+    <Suspense fallback={<div className="spinner" />}>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={React.lazy(() => import("./screens/Home"))}
+          />
+          <Route
+            exact
+            path="/Crew"
+            component={React.lazy(() => import("./screens/Crew"))}
+          />
+          <Route
+            exact
+            path="/Directors"
+            component={React.lazy(() => import("./screens/Directors"))}
+          />
+          <Route
+            exact
+            path="/Philanthropy"
+            component={React.lazy(() => import("./screens/Philanthropy"))}
+          />
+          <Route
+            exact
+            path="/Technology"
+            component={React.lazy(() => import("./screens/Technology"))}
+          />
+          <Route
+            exact
+            path="/Synopsis"
+            component={React.lazy(() => import("./screens/Synopsis"))}
+          />
+          <Route
+            exact
+            path="/Treasure"
+            component={React.lazy(() => import("./screens/Treasure"))}
+          />
+        </Switch>
+      </Router>
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
