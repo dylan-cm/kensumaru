cd4a14f9e554ac27247eca45aa5a4df3e3045471
Dylan Modell <dylan.modell.promo@gmail.com>
2021-03-06 02:02:44 -0800

    fixed layout bug

--- src/molecules/WebNav.tsx
+++ src/molecules/WebNav.tsx
@@ -1,11 +1,72 @@
-import "./WebNav.css"
+import "./WebNav.css";
+import { Link, useLocation, useRouteMatch } from "react-router-dom";
 
 const WebNav = () => {
+  let { url } = useRouteMatch();
+  let location = useLocation();
+
   return (
     <div className="web-nav-container">
-      
+      <Link className="logo" to="/">
+        <h1>KENSU MARU</h1>
+      </Link>
+      <div className="links">
+        <Link
+          className={
+            "nav-link" +
+            (location.pathname === `${url}/Synopsis` ? " active" : "")
+          }
+          to="/Synopsis"
+        >
+          Synopsis
+        </Link>
+        <Link
+          className={
+            "nav-link" +
+            (location.pathname === `${url}/Treasure` ? " active" : "")
+          }
+          to="/Treasure"
+        >
+          Treasure
+        </Link>
+        <Link
+          className={
+            "nav-link" +
+            (location.pathname === `${url}/Directors` ? " active" : "")
+          }
+          to="/Directors"
+        >
+          Directors
+        </Link>
+        <Link
+          className={
+            "nav-link" + (location.pathname === `${url}/Crew` ? " active" : "")
+          }
+          to="/synopsis"
+        >
+          Crew
+        </Link>
+        <Link
+          className={
+            "nav-link" +
+            (location.pathname === `${url}/Technology` ? " active" : "")
+          }
+          to="/synopsis"
+        >
+          Technology
+        </Link>
+        <Link
+          className={
+            "nav-link" +
+            (location.pathname === `${url}/Philanthropy` ? " active" : "")
+          }
+          to="/synopsis"
+        >
+          Philanthropy
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WebNav
+export default WebNav;
