3cd0dfc32de927b5832dee9f61901a8ad6e3c03a
Dylan Cortez-Modell <dylan.modell@gmail.com>
2021-03-11 12:57:04 -0800

    disclaimers added

--- src/molecules/Footer.tsx
+++ src/molecules/Footer.tsx
@@ -3,14 +3,20 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
+    <>
     <div className="footer-container">
       <span>KensuMaru.com</span>
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVLHvAW4hAcxD3qZkmbg9afIKw3e9fLeT0Fi0sY2EgTU9Uqg/viewform">
-        Signup for Updates
+        Sign Up for Updates
       </a>
       <a href="mailto:charles@kensumaru.com">Contact</a>
       <EasterEgg />
     </div>
+    <div className="disclaimer">
+      <span>© Copyright 2020 Kensu Group</span>
+      <span>*Map not to be used for navigation or treasure hunting purposes.</span>
+    </div>
+    </>
   );
 };
 
