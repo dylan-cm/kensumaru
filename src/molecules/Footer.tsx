3536ad827b9e8833ffc5af431479b7e4daa61be6
Dylan Modell <dylan.modell.promo@gmail.com>
2021-03-11 10:53:33 -0800

    signup form and mobile footer improvements

--- src/molecules/Footer.tsx
+++ src/molecules/Footer.tsx
@@ -5,8 +5,11 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <span>KensuMaru.com</span>
-      <EasterEgg />
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVLHvAW4hAcxD3qZkmbg9afIKw3e9fLeT0Fi0sY2EgTU9Uqg/viewform">
+        Signup for Updates
+      </a>
       <a href="mailto:charles@kensumaru.com">Contact</a>
+      <EasterEgg />
     </div>
   );
 };
